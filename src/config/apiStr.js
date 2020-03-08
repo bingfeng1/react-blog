b6bf2e2e603d58eadf3e5f77364a8fb0c8f6ee48
wuxiaoming <260895778@qq.com>
2020-03-08 12:52:55 +0800

    标记点

--- src/config/apiStr.js
+++ src/config/apiStr.js
@@ -1,11 +1,4 @@
-// 后端接口字符串
-// 正式发布，需要将BASE_URL改为内网具体地址
-// const BASE_URL = 'http://192.168.1.50:9000/api/blog'
-// const hostname = document.location.hostname
 let BASE_URL = '/api/blog'
-// if (!(hostname.startsWith('192.168.1') || hostname === 'localhost')) {
-//     BASE_URL = 'http://192.168.1.50:9000' + BASE_URL
-// }
 
 export {
     BASE_URL
